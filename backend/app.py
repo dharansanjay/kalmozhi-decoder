da61626a6c0d73ecb6609a42a2bc9d82fd8e4321
dharansanjay <dharansanjay@60@gmail.com>
2026-04-26 20:15:41 +0530

    debug env check

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 import google.generativeai as genai
+print("KEY:", os.getenv("EMINI_API_KEY"))
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 from flask import Flask, request, jsonify
